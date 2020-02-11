4e8060fe9b013017fb22c8c94168061478c5a48f
kpeca <40670357+kpeca@users.noreply.github.com>
2020-02-11 03:59:07 +0100

    added logo to menu-item component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './header.styles.scss';
-import {Link, BrowserRouter} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import { ReactComponent as Logo} from '../../assets/crown.svg';
 
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './menu-item.styles.scss';
 import {withRouter} from 'react-router-dom';
+import { ReactComponent as Logo} from '../../assets/crown.svg';
 
 const MenuItem = ({title,imageUrl, size, history,linkUrl, match}) => (
     <div  className={`${size} menu-item`} onClick ={ () => history.push(`${match.url}${linkUrl}`)}
@@ -11,6 +12,7 @@ const MenuItem = ({title,imageUrl, size, history,linkUrl, match}) => (
             }}   
         />
                 <div className='content'>
+                <Logo className='logo'></Logo>
                     <h1 className='title'>{title.toUpperCase()}</h1>
                     <span className='subtitle'>SHOP NOW</span>               
                 </div>
