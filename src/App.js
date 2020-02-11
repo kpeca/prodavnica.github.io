9618e51dc59ffee8d3a560361e170975cc79f2a9
kpeca <40670357+kpeca@users.noreply.github.com>
2020-02-11 03:18:03 +0100

    all fixed

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import './App.css';
 import {BrowserRouter,Switch, Route} from 'react-router-dom';
  import HomePage from './pages/homepage/homepage.component';
  import ShopPage from './pages/shop/shop.component';
+  import Header from './components/header/header.component.jsx';
+ 
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div>
-      <BrowserRouter>
+      <Header />
+      
       <Switch>
         <Route exact path ='/' component ={HomePage} />
-        <Route path='/shop' component = {ShopPage} />
+        <Route exact path='/shop' component = {ShopPage} />
       </Switch>
-      </BrowserRouter>
+      
     </div>
+    </BrowserRouter>
   );
 }
 
